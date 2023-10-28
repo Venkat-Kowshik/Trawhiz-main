98f7cdba8189d7072134b1f3053fb9719b7eadd8
Kowshik <kowshikgowrakkavari777@gmail.com>
2023-10-29 00:17:28 +0530

    Gta website update

---
+++ Trawhiz/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        
+      </div>
+     
+      <div className="card">
+      
+      </div>
+     
+    </>
+  )
+}
+
+export default App
