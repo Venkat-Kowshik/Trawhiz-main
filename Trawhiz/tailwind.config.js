fc3f786b9b04ec497d1f20083d213087679e0340
Kowshik <kowshikgowrakkavari777@gmail.com>
2023-11-04 00:18:22 +0530

    Update

--- Trawhiz/tailwind.config.js
+++ Trawhiz/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html","./main.js"],
   theme: {
     extend: {
+      screens:{
+        xs:"425px",
+      },
       colors:{
         bkg: "#171819",
         muted: "#C9CBCF",
